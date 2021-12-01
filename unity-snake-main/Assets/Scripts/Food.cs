53a4a1b777aebb775fd94eb97f8c59c76cca180f
saboop99 <64094846+saboop99@users.noreply.github.com>
2021-11-30 22:53:33 -0300

    Comentários na classe Food.cs

--- unity-snake-main/Assets/Scripts/Food.cs
+++ unity-snake-main/Assets/Scripts/Food.cs
@@ -6,20 +6,20 @@ public class Food : MonoBehaviour
 
     private void Start()
     {
-        //
+        //Chamando o método RandomizePosition, que como o nome diz define uma posição aleatória
         RandomizePosition();
     }
 
     public void RandomizePosition()
     {
-        //
+        //Criado variavel bounds tipo Bounds, que define o tamanho da area em que aparecem aletoriamente as comidas
         Bounds bounds = this.gridArea.bounds;
 
-        //
+        //utilização dos floats dos eixos x e y, que definem que a comida ira aparecer entre o mínimo de um eixo e o máximo de outro
         float x = Random.Range(bounds.min.x, bounds.max.x);
         float y = Random.Range(bounds.min.y, bounds.max.y);
 
-        // 
+        //chamando as duas posições (x e y) para formar um novo ponto para criação da food
         x = Mathf.Round(x);
         y = Mathf.Round(y);
 
@@ -28,7 +28,7 @@ public class Food : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        //
+        //Chamando o método RandomizePosition(após a colisão)
         RandomizePosition();
     }
 
